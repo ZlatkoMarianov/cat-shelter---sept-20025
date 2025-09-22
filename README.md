b75cd60fa81d82f2afd91bb5d61546e21e7eeeb2
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-09-22 20:11:37 +0300

    create addBreed page

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # cat-shelter---sept-20025
+
 SoftUni JS Back End Course Project
 
 ## ToDos:
- - [x] Initialize NodeJS Project
- - [x] Dev script
- - [x] Debugging
- - [x] Add basic http server
- - [x] Add resources
- - [x] Show home page
- - [x] Add css
- - [x] Read home html from file sistem
- - [] 
- - [] 
+
+- [x] Initialize NodeJS Project
+- [x] Dev script
+- [x] Debugging
+- [x] Add basic http server
+- [x] Add resources
+- [x] Show home page
+- [x] Add css
+- [x] Read home html from file sistem
+- [x] Read site css from file system
+- [] Add create bread page
+- [] Add create cat page
+- []
+- []
+- []
+- []
--- src/index.js
+++ src/index.js
@@ -1,24 +1,48 @@
 import http from 'http';
 import fs from 'fs/promises';
 
-import siteCss from './site.css.js';
+async function homeView() {
+   const homeHtml = await fs.readFile('./src/views/home/index.html', { encoding: 'utf-8' });
+   return homeHtml;
+}
+
+async function addBreedView() {
+   const html = await fs.readFile('./src/views/addBreed.html', { encoding: 'utf-8' });
+   return html;
+}
+
 
 const server = http.createServer(async (req, res) => {
-   if (req.url === '/') {
-      const homeHtml = await fs.readFile('./src/views/home/index.html', { encoding: 'utf-8' });
+   let html;
 
-      res.writeHead(200, {
-         'content-type': 'text/html'
-      });
+   switch (req.url) {
+      case '/':
+         html = await homeView();
+         break;
 
-      res.write(homeHtml);
-   } else if (req.url === '/styles/site.css') {
-      res.writeHead(200, {
-         'content-type': 'text/css',  
-      });
-      res.write(siteCss);
+      case '/cats/add-breed':
+         html = await addBreedView();
+         break;
+
+      case '/styles/site.css':
+         const siteCss = await fs.readFile('./src/content/styles/site.css', { encoding: 'utf-8' });
+
+         res.writeHead(200, {
+            'content-type': 'text/css',
+         });
+         res.write(siteCss);
+         res.end();
+         return;
+
+      default:
+         return res.end();
    }
 
+   res.writeHead(200, {
+      'content-type': 'text/html'
+   });
+
+   res.write(html);
    res.end();
 });
 
